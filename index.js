9f826d46c526aa9c0b65b88620af405d00809fb8
Roshan <shahroshan0720@gmail.com>
2023-10-04 12:29:08 +0530

    config setup

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const connectDB = require('./config/db');
+const Pusher = require('pusher');
 
 const app = express();
+
+const pusher = new Pusher({
+    appId: process.env.APPID,
+    key: process.env.KEY,
+    secret: process.env.SECRET,
+    cluster: "ap2",
+});
 
 app.set('views', path.join(__dirname, 'views'));
 require('hbs').registerHelper('toJson', data => JSON.stringify(data));
@@ -14,6 +23,15 @@ app.get('/',(req,res)=>{
 })
 const PORT = process.env.PORT || 2323;
 
-app.listen(PORT ,()=>{
-    console.log(`Server listening at http://localhost:${PORT}`);
-});
+const start = async()=>{
+    try{
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT ,()=>{
+            console.log(`Server listening at http://localhost:${PORT}`);
+        });
+    }catch(err){
+        console.log('Error in connecting to server');
+    } 
+}
+
+start();
